e4b4e92ae35c3436bd6cd02d649e06d3fb431c40
Vernny Chavez <v3r.dev@gmail.com>
2024-04-11 07:46:18 -0300

    Criação do projeto

---
+++ CTGerator.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+conn = sqlite3.connect('database_calculus.db')
+
+cur = conn.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS alunos (nome TEXT, nota INTEGER)''')
+
+dados_alunos = [('Carlos', 8), ('João', 9)]
+
+cur.executemany('INSERT INTO alunos (nome, nota) VALUES (?, ?)', dados_alunos)
+
+conn.commit()
+
+print('Dados inseridos com sucesso.')
+
+conn.close()
+
+
+
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+    pass

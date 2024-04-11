210f7ef3a311db1f64d43ac07c8612d7819f8c9a
Vernny Chavez <v3r.dev@gmail.com>
2024-04-11 11:01:06 -0300

    Primeira tentativa de organização em funções

--- CTGerator.py
+++ CTGerator.py
@@ -1,21 +1,119 @@
 import sqlite3
 
-conn = sqlite3.connect('database_calculus.db')
+def criarBancoDados(NomeBancoDados):
+    # Conectar-se ao banco de dados
+    conn = sqlite3.connect(NomeBancoDados + '.db')
+    cur = conn.cursor()
 
-cur = conn.cursor()
+    # Criar a tabela se ela não existir
+    cur.execute('''CREATE TABLE IF NOT EXISTS NomeBancoDados (enunciado TEXT, idTopico TEXT)''')
 
-cur.execute('''CREATE TABLE IF NOT EXISTS alunos (nome TEXT, nota INTEGER)''')
+    # Cometer as alterações e fechar a conexão
+    conn.commit()
+    conn.close()
 
-dados_alunos = [('Carlogit push s', 8)]
 
-cur.executemany('INSERT INTO alunos (nome, nota) VALUES (?, ?)', dados_alunos)
+def inserirLista(NomeBancoDados, lista):
+    # Conectar-se ao banco de dados
+    conn = sqlite3.connect(NomeBancoDados + '.db')
+    cur = conn.cursor()
 
-conn.commit()
+    # Inserir a lista na tabela
+    cur.executemany(f'INSERT INTO {NomeBancoDados} (enunciado, idTopico) VALUES (?, ?)', lista)
 
-print('Dados inseridos com sucesso.')
+    # Cometer as alterações e fechar a conexão
+    conn.commit()
+    conn.close()
+    pass
 
-conn.close()
+def inserirDados(NomeBancoDados, lista):
+    # Conectar-se ao banco de dados correto
+    conn = sqlite3.connect(NomeBancoDados + '.db')
+    cur = conn.cursor()
 
+    # Coletar enunciado e tópico da questão
+    inicio = r'''
+    \documentclass{article}
+    \begin{document}
+    '''
+    final = r'''
+    \end{document}
+    '''
+    enunciado = input('Quesão: ')
+    idTopico = input('Informe o tópico da questão: ')
+    lista = [(enunciado, idTopico)]
+
+    # Inserir dados na tabela
+    cur.execute('INSERT INTO NomeBancoDados (enunciado, idTopico) VALUES (?, ?)', lista)
+
+    # Cometer as alterações e fechar a conexão
+    conn.commit()
+    conn.close()
+
+    print('Dados inseridos com sucesso.')
+
+def inserirDados2(NomeBancoDados):
+    conn = sqlite3.connect('NomeBancoDados.db')
+    cur = conn.cursor()
+
+    print(r"""
+    \documentclass{article}
+    \begin{document}""")
+    enunciado = str(input(r"""
+    Questão: """))
+    print(r"""
+    \end{document}
+          """)
+    idTopico = str(input('''Informe o tópico da questão: '''))
+
+    dados = [(enunciado, idTopico)]
+
+    cur.executemany('INSERT INTO NomeBancoDados (enunciado, idTopico) VALUES (?, ?)', dados)
+    conn.commit()
+    print('Dados inseridos com sucesso.')
+    conn.close()
+    pass
+
+def imprimirDados(NomeBancoDados):
+    # Conectar-se ao banco de dados
+    conn = sqlite3.connect(NomeBancoDados + '.db')
+    cur = conn.cursor()
+
+    # Selecionar todos os dados da tabela
+    cur.execute('SELECT * FROM NomeBancoDados')
+
+    # Iterar sobre os resultados e imprimir cada linha
+    for linha in cur.fetchall():
+        print(linha)
+
+    # Fechar a conexão
+    conn.close()
+
+def imprimirNomesTabelas(NomeBancoDados):
+    # Conectar ao banco de dados
+    conn = sqlite3.connect(NomeBancoDados)
+    cursor = conn.cursor()
+
+    # Executar a consulta SQL para recuperar os nomes das tabelas
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+
+    # Recuperar os nomes das tabelas do cursor
+    NomesTabelas = cursor.fetchall()
+
+    # Imprimir os nomes das tabelas
+    print("Nomes das tabelas:")
+    for tabela in NomesTabelas:
+        print(tabela[0])
+
+    # Fechar a conexão com o banco de dados
+    conn.close()
+
+
+
+#criarBancoDados('BancoTeste3')
+#inserirDados('BancoTeste3')
+#imprimirDados('BancoTeste3')
+imprimirNomesTabelas('BancoTeste3')
 
 
 class Question:
